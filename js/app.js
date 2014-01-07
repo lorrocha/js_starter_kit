2521611ad135c431b21a36a8161cac7fa05c4241
Lorry Rocha <sorai120@yahoo.com>
2014-01-07 13:08:58 -0500

    After much blood sweat and tears, I got to store the input upon Enter

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-alert('Hey find me to know where to put the javascript');
+var answer = Math.floor(Math.random()*(15 + 1));
+var userGuess = 0;
+var input = document.getElementById("input-box");
+
+
+$('.alert-box').html('Hey! Why not guess a number between 0 and 15?');
+
+function getUserGuess() {
+  userGuess = document.getElementById("input-box").value;
+  document.getElementById("input-box").value="";
+  console.log(userGuess);
+};
+
+var submitGuess = function() {
+  $("#input-box").keypress(function (e) {
+    var key = window.event ? e.keyCode : e.which;
+    if (key == '13') {
+      getUserGuess();
+    }
+  })
+};
+
+
+// NEEDS THIS TO TRACK/REPEAT THE KEYPRESS TEXT
+input.addEventListener("keypress", submitGuess, true)
+
