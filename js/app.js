a7dd93a70ba9cf0ef9484e4614e421cb7e6df559
Lorry Rocha <sorai120@yahoo.com>
2014-01-07 17:32:42 -0500

    Got rid of my old gross code and replaced it with funky new ones! And fewer event listeners

--- js/app.js
+++ js/app.js
@@ -1,26 +1,30 @@
-var answer = Math.floor(Math.random()*(15 + 1));
+var answer = Math.floor(Math.random()*(2 + 1));
 var userGuess = 0;
 var input = document.getElementById("input-box");
 
-
-$('.alert-box').html('Hey! Why not guess a number between 0 and 15?');
-
-function getUserGuess() {
-  userGuess = document.getElementById("input-box").value;
-  document.getElementById("input-box").value="";
-  console.log(userGuess);
-};
-
 var submitGuess = function() {
-  $("#input-box").keypress(function (e) {
-    var key = window.event ? e.keyCode : e.which;
-    if (key == '13') {
+  $("#input-box").keypress(function(e) {
+    if (e.keyCode == '13') {
       getUserGuess();
     }
   })
 };
 
+var compareGuess = function(num) {
+  if (answer == num) {
+    $(".alert-box").html('So rad!');
+  } else {
+    $(".alert-box").html('Woops! Try again!' + num);
+  }
+}
 
-// NEEDS THIS TO TRACK/REPEAT THE KEYPRESS TEXT
-input.addEventListener("keypress", submitGuess, true)
+var getUserGuess = function() {
+  guess =  $('#input-box').val();
+  compareGuess(guess);
+  $('#input-box').val('');
+};
+
+$(document).ready(function(){
+  submitGuess();
+})
 
