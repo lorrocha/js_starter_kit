8a8e30f6ac906d8a481479de9f51ee20e51e2e99
Lorry Rocha <sorai120@yahoo.com>
2014-01-07 17:56:32 -0500

    Full functionality. Included a total count counter

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var answer = Math.floor(Math.random()*(2 + 1));
-var userGuess = 0;
+var answer = Math.floor(Math.random()*(20 + 1));
+var guessCount = 0;
 var input = document.getElementById("input-box");
 
 var submitGuess = function() {
@@ -10,11 +10,20 @@ var submitGuess = function() {
   })
 };
 
-var compareGuess = function(num) {
-  if (answer == num) {
-    $(".alert-box").html('So rad!');
+var guesstimate = function(num) {
+  if (num > answer) {
+    return ' is too high!';
   } else {
-    $(".alert-box").html('Woops! Try again!' + num);
+    return ' is too low!';
+  }
+};
+
+var compareGuess = function(num) {
+  guessCount++
+  if (answer == num) {
+    $(".alert-box").append('<br><div><b>So rad! You totally won!!</b><br>You won in '+ guessCount + ' tries!</div>');
+  } else {
+    $(".alert-box").append('<div>Woops! Try again! ' + num + guesstimate(num) + '</div>');
   }
 }
 
